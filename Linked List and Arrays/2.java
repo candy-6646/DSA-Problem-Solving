736212295ce7939100d82f335f923b6951599f17
unknown <kundanjha826@gmail.com>
2022-09-24 22:37:51 +0530

    Solved LL problems

--- Linked List and Arrays/2.java
+++ Linked List and Arrays/2.java
@@ -48,3 +48,52 @@ class Solution {
         return newHead;
     }
 }
+
+
+
+
+
+/**************************Optimized in linear time solution**********************************/
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        Node temp = head;
+        
+        //inserting new Nodes in between original LL
+        while(temp != null) {
+            Node newNode = new Node(temp.val);
+            
+            Node next = temp.next;
+            newNode.next = temp.next;
+            temp.next = newNode;
+            temp = next;
+        }
+        
+        //now setting random ptr of nodes
+        temp = head;
+        while(temp != null) {
+            Node random = temp.random;
+            if(random == null) {
+                temp.next.random = null;
+            }else
+                temp.next.random = random.next;
+            
+            temp = temp.next.next;
+        }
+        
+        //seperating Lists
+        Node t1 = head;
+        Node t2 = head.next;
+        head = head.next;
+        while(t1 != null) {
+            t1.next = t1.next.next;
+            if(t2.next != null)
+                t2.next = t2.next.next;
+            
+            t1 = t1.next;
+            t2 = t2.next;
+        }
+        
+        return head;
+    }
+}
