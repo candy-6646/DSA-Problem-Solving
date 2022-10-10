8a9f8e0efe42b65f8d4e3b05c8fdb44bd580c9bc
unknown <kundanjha826@gmail.com>
2022-10-10 23:33:58 +0530

    Rabin Karp Algo

--- String/6.java
+++ String/6.java
@@ -7,3 +7,44 @@ Best case for this Algo. is that now other vals. found similar to hashVal of our
 worst is O(N^2)
 */
 
+
+import java.util.ArrayList;
+
+public class Solution {
+	public static ArrayList<Integer> stringMatch(String str, String pat) {
+		ArrayList<Integer> list = new ArrayList<>();
+        //Rabin Karp
+        //Step-1: Generate a hashCode to pat
+        int pCode = 0;
+        for(int i = 0; i < pat.length(); i++) {
+            char ch = pat.charAt(i);
+            pCode = pCode + (ch - 'A');
+        }
+        
+        int sCode = 0;
+        for(int i = 0; i < pat.length(); i++) {
+            sCode = sCode + (str.charAt(i) - 'A');
+        }
+        if(pCode == sCode && isSubstring(str, pat, 0)) list.add(0);
+        
+        for(int i = pat.length(); i < str.length(); i++) {
+            //remove last and add new char
+            int lastIdx = i-pat.length();
+            sCode = sCode - (str.charAt(lastIdx) - 'A');
+            sCode = sCode + (str.charAt(i) - 'A');
+    
+           if(pCode == sCode && isSubstring(str, pat, lastIdx+1)) 
+               list.add(lastIdx + 1);
+        }
+        
+        return list;
+	}
+    
+    static boolean isSubstring(String str, String pat, int idx) {
+        for(int j = 0; j < pat.length(); j++) {
+            if(str.charAt(idx+j) != pat.charAt(j)) return false;
+        }
+        
+        return true;
+    }
+}
